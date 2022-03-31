8c04e5433dfd0b72004394cc1da6215a4e1762a5
Miriam Di Giandomenico <miriam.digiandomenico@gmail.com>
2022-03-31 16:34:21 +0200

    first commit

---
+++ Hello.java
@@ -0,0 +1,21 @@
+// Hello.java
+public class Hello {
+	public static void main(String[] args) {
+		System.out.println("Hello, Miriam!");
+	}
+}
+
+// sono per i commenti su una riga! 
+/* per commenti multirighe
+
+ho creato una classe con un  metodo che si chiama main, con una classe println per stampare un messaggio.
+type sul terminale + nome file ci permette di vederne il contenuto
+
+OGNI VOLTA che modifico il file originale va usato il compilatore javac + nome file.
+
+il metodo main c'è sempre. il suo scopo è essere invocato
+public = chiunque ha accesso al file può accedere alla classe
+se c'è un errore nella riga ma la riga è corretta allora solitamente l'errore è nella riga prima!!!
+*/
+/* il parametro args è un array di stringhe
+*/
